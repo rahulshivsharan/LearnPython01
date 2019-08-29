245d3c18358f2a4dd21c9ce1d10790ece99e7baf
Rahul Shivsharan <postbox.rahul@hotmail.com>
2019-08-29 18:53:12 +0530

    Sorting map (dictionary) by key's and return as list of tuple's
    C:\>python ex05.py
    [(3, 'Abott Hamilton'), (8, 'Xander Charlton'), (11, 'Viktor Lutof')]
    
    C:\>

---
+++ ex07.py
@@ -0,0 +1,19 @@
+def func():
+    map = {
+		11 : "Viktor Lutof",
+        8 : "Xander Charlton",
+		3 : "Abott Hamilton"
+	}
+
+    # defining inner function which returns key from dictionary
+    def fn(keyAndVal):
+        return keyAndVal[0]
+
+    # passing  the above function to the key attribute for 'sorted' function
+    # which returns list of tuple's,
+    # the list is sorted as per key 
+    sortedList = sorted(map.items(), key = fn)
+	
+    print(sortedList);
+
+func()
