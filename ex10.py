95e9ad7d936b28b4071fd8d7d587ac58e0a55f61
Rahul Shivsharan <postbox.rahul@hotmail.com>
2019-08-29 19:36:26 +0530

    Sorting map according to values
    C:\>python ex05.py
    [(3, 'Abott Hamilton'), (11, 'Viktor Lutof'), (8, 'Xander Charlton')]
    
    C:\>python ex05.py
    [(3, 'Abott Hamilton'), (11, 'Viktor Lutof'), (8, 'Xander Charlton')]
    3  ->  Abott Hamilton
    11  ->  Viktor Lutof
    8  ->  Xander Charlton
    
    C:\>

---
+++ ex10.py
@@ -0,0 +1,27 @@
+import collections
+
+def func():
+    map = {
+		    11 : "Viktor Lutof",
+        8 : "Xander Charlton",
+		    3 : "Abott Hamilton"
+	  }
+
+    # defining inner function which returns value from dictionary
+    def fn(keyAndVal):
+        return keyAndVal[1]
+
+    # passing  the above function to the key attribute for 'sorted' function
+    # which returns list of tuple's,
+    # the list is sorted as per value 
+    sortedList = sorted(map.items(), key = fn)
+	
+    print(sortedList);
+	
+    map = collections.OrderedDict(sortedList)
+
+    for k,v in map.items():
+        print(k," -> ",v)	
+
+
+func()
