8c16f1c994e0828996624b00233347ebb61fde6f
Rahul Shivsharan <postbox.rahul@hotmail.com>
2019-08-29 17:53:46 +0530

    Usage of Map's item() method
    C:\>python ex05.py
    NAME ->  Viktor Lutof
    GENDER ->  MALE
    AGE ->  47
    
    C:\>

---
+++ ex06.py
@@ -0,0 +1,11 @@
+def func():
+    map = {
+        "NAME" : "Viktor Lutof",
+        "GENDER" : "MALE",
+        "AGE" : 47		
+	}
+
+    for key,val in map.items():
+        print(key+" -> ",val)	
+
+func()
