d96714b4c166247ff4a7acee4e8c34e2e468ba91
Rahul Shivsharan <postbox.rahul@hotmail.com>
2019-09-12 15:29:34 +0530

    Using List as a stack 'pop' and 'push' operation
    C:\Users\shivrah\python_practise>python ex08.py
    ['ONE', 'TWO', 'THREE']
    1st pop from stack  THREE
    after pop  ['ONE', 'TWO']
    after append  ['ONE', 'TWO', 'FOUR']
    
    C:\Users\shivrah\python_practise>

---
+++ ex14.py
@@ -0,0 +1,9 @@
+def fun01():
+    fts = ["ONE","TWO","THREE"]
+    print(fts)
+    print("1st pop from stack ",fts.pop())
+    print("after pop ",fts)
+    fts.append("FOUR")
+    print("after append ",fts)
+
+fun01()
