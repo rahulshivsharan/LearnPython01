8902690f642f78c6f3209a4cf38232971d2944b1
Rahul Shivsharan <postbox.rahul@hotmail.com>
2019-09-17 20:05:36 +0530

    Usage of set, no duplicates allowed
    C:\python_practise>python ex08.py
    {'TWO', 'FOUR', 'THREE', 'ONE'}
    
    C:\python_practise>

---
+++ 21.py
@@ -0,0 +1,8 @@
+# Iterating through 2D matrix
+def fun01():
+    basket = {"ONE", "TWO", "THREE"}
+    basket.add("ONE") # no duplicates allowed
+    basket.add("FOUR")    
+    print(basket)
+    
+fun01()
