7b4114797d01865d703315ad311790ecc73c744d
Rahul Shivsharan <postbox.rahul@hotmail.com>
2019-09-19 16:22:25 +0530

    Iterate dictionay using method 'items'
    C:\>python ex08.py
    [Key]  2341 , [Value]  jack
    [Key]  4356 , [Value]  ron
    [Key]  1265 , [Value]  swet
    
    C:\>

---
+++ ex26.py
@@ -0,0 +1,15 @@
+
+def fun01():
+    # intialising dictionary
+    tel = {
+        2341 : 'jack',
+        4356 : 'ron',
+        1265 : 'swet' 
+    }
+
+    for k,v in tel.items():
+        print("[Key] ",k,", [Value] ",v)	
+		
+
+
+fun01()
